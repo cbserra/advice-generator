8b6315fdfefd92727e668a8b7324fa18e089f137
Chris Serra <789723+cbserra@users.noreply.github.com>
2022-05-03 03:45:53 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import diceIcon from './images/icon-dice.svg'
+import useAxios from 'axios-hooks';
 
 function App() {
+  
+  const [{ response, loading, error }, refetch] = useAxios({
+    method: 'get',
+    url: 'https://api.adviceslip.com/advice',
+  });
+
+  const [advice, setAdvice] = useState<string>("")
+
+  React.useEffect(() => { 
+    if (response != null) {
+      setAdvice(response.data.slip.advice)
+    }
+  },
+  [response])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="advice-header">
+        Advice #1111
       </header>
+      <main className="advice-container">
+        {loading 
+          ? (<div className="info-message">
+                <i className="fa fa-info-circle"></i>
+                Loading...
+            </div>)
+          :  error && (
+                  <div className="error-message">
+                      <i className="fa fa-times-circle"></i>
+                      {error.message}
+                  </div>
+                )
+          }
+          {   advice && (
+                <div className="advice-quote">
+                  {advice}
+                </div>
+              )
+            }
+      </main>
+      <footer>
+        <div className="advice-divider"></div>
+        <div className="next-quote" onClick={() => refetch()}>
+          <img src={diceIcon} alt="next quote"/>
+        </div>
+      </footer>
     </div>
   );
 }
