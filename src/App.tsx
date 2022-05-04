95b8cac3d3d6fdfa857e645569869b4a0ae35fcd
Chris Serra <789723+cbserra@users.noreply.github.com>
2022-05-04 08:10:10 -0700

    Finally got elements playing well with window resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import diceIcon from './images/icon-dice.svg'
 import useAxios from 'axios-hooks';
+import desktopDivider from './images/pattern-divider-desktop.svg';
+import mobileDivider from './images/pattern-divider-mobile.svg';
 
 export interface Slip {
   id: number,
   advice: string
 }
 
- const App = () => {
+const App = () => {
   
   const [{ response, loading, error }, refetch] = useAxios({
     method: 'get',
@@ -25,37 +26,41 @@ export interface Slip {
 
   return (
     <div className="App">
-      { slip && (
-      <header className="advice-header">
-        Advice #{slip.id}
-      </header>
-      )}
-      <main className="advice-container">
-        {loading 
-          ? (<div className="loading-message">
+      <main>
+      { 
+      (
+        <h1>Advice { slip ? `#${slip.id}` : '...' }</h1>
+      )
+      }
+      { loading 
+        ? (
+          <div className="loading-message">
               <i className="fa-solid fa-spinner fa-spin-pulse"></i>
               Loading...
-            </div>)
-          :  error && (
-                  <div className="error-message">
-                      <i className="fa fa-times-circle"></i>
-                      {error.message}
-                  </div>
-                )
-          }
-          {   slip && (
-                <div className="advice-quote">
-                  {slip.advice}
-                </div>
-              )
-            }
-      </main>
-      <footer>
-        <div className="advice-divider"></div>
+          </div>
+          )
+        : error && 
+          (
+          <div className="error-message">
+            <i className="fa fa-times-circle"></i>
+            {error.message}
+          </div>
+          )
+        }
+        {  
+        (
+        <q className="advice-quote">
+          { (!loading && slip) ? slip.advice : '...' }
+        </q>
+        )
+        }
+        <picture>
+          <source srcSet={mobileDivider} media="(min-width: 375px) and (max-width: 999px)" />
+          <img src={desktopDivider} alt="divider" />
+        </picture>
         <div className="next-quote" onClick={() => refetch()}>
-          <img src={diceIcon} alt="next quote"/>
         </div>
-      </footer>
+      </main>
     </div>
   )
 }
